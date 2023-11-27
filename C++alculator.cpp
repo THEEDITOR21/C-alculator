d1ecad84c3ea5211c394d3cf3ada6123e8757cbd
THEEDITOR21 <152213487+THEEDITOR21@users.noreply.github.com>
2023-11-27 15:14:51 -0500

    Add files via upload

---
+++ C++alculator.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<stdlib.h>
+using namespace std;
+
+int main(){
+	
+		system("color 1");
+		cout<<"_____________________________________"<<endl;
+		cout<<"|            Calculator             |"<<endl;
+		cout<<"| Made by TheEditor and [REDACTED]  |"<<endl;
+		cout<<"|   ENJOY (Or be put to sleep...)   |"<<endl;
+		cout<<"-------------------------------------"<<endl;
+		double num1;
+		double num2;
+		char op;
+		system("color 1");
+		while(true){
+			system("color 2");
+		cin >> num1;
+		cin >> op;
+		cin >> num2;
+		if(op=='+') {
+		cout << num1+num2 <<endl;
+		};
+		if(op=='-'){
+		cout << num1-num2 <<endl;
+		};
+		if(op=='/'){
+		cout << num1/num2 <<endl;
+		};
+		if(op=='*'){
+		cout << num1*num2 <<endl;
+	};
+	}
+}
